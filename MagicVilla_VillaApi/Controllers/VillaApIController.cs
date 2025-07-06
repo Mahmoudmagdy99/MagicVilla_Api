1c54f18cf150241e774e1cce8d87166274c2adda
mahmoud magdy <mahmoudkotamy97@gmail.com>
2025-07-07 02:20:40 +0300

    apply first end point

--- MagicVilla_VillaApi/Controllers/VillaApIController.cs
+++ MagicVilla_VillaApi/Controllers/VillaApIController.cs
@@ -1,6 +1,24 @@
-﻿namespace MagicVilla_VillaApi.Controllers
+﻿using MagicVilla_VillaApi.Data;
+using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaApi.Controllers
 {
-    public class VillaApIController
+    //[Route("api/[controller]")]
+    [Route("api/VillaApI")]
+    [ApiController]
+    public class VillaApIController :ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
     }
 }
---
+++ MagicVilla_VillaApi/Data/VillaStore.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_VillaApi.Models.Dto;
+
+namespace MagicVilla_VillaApi.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO> {
+            new VillaDTO { Id=1, Name="Pool View"},
+            new VillaDTO { Id=2, Name="Beach View"},
+            //new VillaDTO { Id=3, Name="Mountain View"},
+            //new VillaDTO { Id=4, Name="Garden View"},
+            //new VillaDTO { Id=5, Name="City View"}
+        };
+    }
+}
